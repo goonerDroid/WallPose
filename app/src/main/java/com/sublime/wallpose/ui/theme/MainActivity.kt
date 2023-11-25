b4d07b06eeff44491ea045afbfa8068e5633f0d0
williamj <williamj949@gmail.com>
2023-11-25 01:25:35 -0500

    Update MainActivity.kt

---
+++ app/src/main/java/com/sublime/wallpose/ui/theme/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.sublime.wallpose.ui.theme
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.sublime.wallpose.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+//        setContent {
+//            WallPoseTheme {
+//                // A surface container using the 'background' color from the theme
+//                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+//                    Greeting("Android")
+//                }
+//            }
+//        }
+    }
+}
+
+//@Composable
+//fun Greeting(name: String, modifier: Modifier = Modifier) {
+//    Text(
+//            text = "Hello $name!",
+//            modifier = modifier
+//    )
+//}
+//
+//@Preview(showBackground = true)
+//@Composable
+//fun GreetingPreview() {
+//    WallPoseTheme {
+//        Greeting("Android")
+//    }
+//}
